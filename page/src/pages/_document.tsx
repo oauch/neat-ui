66c7ef2af5cde4a11bed9a5cec7c4bc8f62ebee2
oauch <lgm9515@naver.com>
2024-06-14 18:16:28 +0900

    init: homepage

---
+++ page/src/pages/_document.tsx
@@ -0,0 +1,12 @@
+import { Head, Html, Main } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="ko">
+      <Head />
+      <body>
+        <Main />
+      </body>
+    </Html>
+  );
+}

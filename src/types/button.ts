5ad232783f412160114e5ee96ce49062de7981ce
oauch <lgm9515@naver.com>
2024-06-15 18:12:48 +0900

    fix: ref remove

--- src/types/button.ts
+++ src/types/button.ts
@@ -1,13 +1,11 @@
-import { DefaultProps } from '@/types/default';
-import { RefObject } from 'react';
+import { DefaultProps } from "@/types/default";
 
 interface ButtonProps extends DefaultProps {
-  ref?: RefObject<HTMLButtonElement>;
-  type?: 'button' | 'submit';
-  size?: 'sm' | 'md' | 'lg';
+  type?: "button" | "submit";
+  size?: "sm" | "md" | "lg";
   fs?: number;
-  colorTheme?: 'primary' | 'black' | 'white';
-  variants?: 'none' | 'outline';
+  colorTheme?: "primary" | "black" | "white";
+  variants?: "none" | "outline";
   onClick?: () => void;
   isDisabled?: boolean;
 }

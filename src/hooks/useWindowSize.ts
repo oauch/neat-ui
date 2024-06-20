141da875529307abf18901734f1cef5b1cc27820
oauch <lgm9515@naver.com>
2024-06-20 15:29:24 +0900

    fix: Text hydration error

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -1,22 +1,30 @@
+// useWindowSize.js
 import { useEffect, useState } from "react";
 
-const INITIAL_SIZE = { width: 0, height: 0 };
-
-function useWindowSize() {
-  const [windowSize, setWindowSize] = useState(INITIAL_SIZE);
+const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState({
+    width: typeof window !== "undefined" ? window.innerWidth : 0,
+    height: typeof window !== "undefined" ? window.innerHeight : 0,
+  });
 
   useEffect(() => {
-    const onResize = () => {
-      setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
     };
 
-    onResize();
+    if (typeof window !== "undefined") {
+      window.addEventListener("resize", handleResize);
 
-    window.addEventListener("resize", onResize);
-    return () => window.removeEventListener("resize", onResize);
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }
   }, []);
 
   return windowSize;
-}
+};
 
 export default useWindowSize;
